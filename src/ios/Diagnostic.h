04142a2b7907bd552e2b441ec56d29b01c92175f
yunseok <hannut91@gmail.com>
2017-12-11 16:41:50 +0900

    unused code deleted

--- src/ios/Diagnostic.h
+++ src/ios/Diagnostic.h
@@ -9,32 +9,16 @@
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
 #import <WebKit/WebKit.h>
-
-#import <CoreBluetooth/CoreBluetooth.h>
 #import <CoreLocation/CoreLocation.h>
 #import <CoreMotion/CoreMotion.h>
-#import <EventKit/EventKit.h>
-#import <AVFoundation/AVFoundation.h>
 #import <Photos/Photos.h>
-#import <AddressBook/AddressBook.h>
-#import <Contacts/Contacts.h>
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-#import <UserNotifications/UserNotifications.h>
-#endif
+@interface Diagnostic : CDVPlugin <CLLocationManagerDelegate>
 
-#import <arpa/inet.h> // For AF_INET, etc.
-#import <ifaddrs.h> // For getifaddrs()
-#import <net/if.h> // For IFF_LOOPBACK
-
-@interface Diagnostic : CDVPlugin <CBCentralManagerDelegate, CLLocationManagerDelegate>
-
-    @property (nonatomic, retain) CBCentralManager* bluetoothManager;
     @property (strong, nonatomic) CLLocationManager* locationManager;
     @property (strong, nonatomic) CMMotionActivityManager* motionManager;
     @property (strong, nonatomic) NSOperationQueue* motionActivityQueue;
     @property (nonatomic, retain) NSString* locationRequestCallbackId;
-    @property (nonatomic) EKEventStore *eventStore;
     @property (nonatomic, retain) NSString* currentLocationAuthorizationStatus;
 
 - (void) isLocationAvailable: (CDVInvokedUrlCommand*)command;
@@ -51,36 +35,8 @@
 - (void) isCameraRollAuthorized: (CDVInvokedUrlCommand*)command;
 - (void) getCameraRollAuthorizationStatus: (CDVInvokedUrlCommand*)command;
 
-- (void) isWifiAvailable: (CDVInvokedUrlCommand*)command;
-- (void) isWifiEnabled: (CDVInvokedUrlCommand*)command;
-
-- (void) isBluetoothAvailable: (CDVInvokedUrlCommand*)command;
-- (void) getBluetoothState: (CDVInvokedUrlCommand*)command;
-- (void) requestBluetoothAuthorization: (CDVInvokedUrlCommand*)command;
-
-- (void) isRemoteNotificationsEnabled: (CDVInvokedUrlCommand*)command;
-- (void) getRemoteNotificationTypes: (CDVInvokedUrlCommand*)command;
-- (void) isRegisteredForRemoteNotifications: (CDVInvokedUrlCommand*)command;
-
 - (void) switchToSettings: (CDVInvokedUrlCommand*)command;
 - (void) switchToLocationSettings: (CDVInvokedUrlCommand*)command;
-
-- (void) isMicrophoneAuthorized: (CDVInvokedUrlCommand*)command;
-- (void) getMicrophoneAuthorizationStatus: (CDVInvokedUrlCommand*)command;
-- (void) requestMicrophoneAuthorization: (CDVInvokedUrlCommand*)command;
-
-- (void) getAddressBookAuthorizationStatus: (CDVInvokedUrlCommand*)command;
-- (void) isAddressBookAuthorized: (CDVInvokedUrlCommand*)command;
-- (void) requestAddressBookAuthorization: (CDVInvokedUrlCommand*)command;
-
-- (void) getCalendarAuthorizationStatus: (CDVInvokedUrlCommand*)command;
-- (void) isCalendarAuthorized: (CDVInvokedUrlCommand*)command;
-- (void) requestCalendarAuthorization: (CDVInvokedUrlCommand*)command;
-- (void) getRemindersAuthorizationStatus: (CDVInvokedUrlCommand*)command;
-- (void) isRemindersAuthorized: (CDVInvokedUrlCommand*)command;
-- (void) requestRemindersAuthorization: (CDVInvokedUrlCommand*)command;
-
-- (void) getBackgroundRefreshStatus: (CDVInvokedUrlCommand*)command;
 
 - (void) isMotionAvailable: (CDVInvokedUrlCommand*)command;
 - (void) isMotionRequestOutcomeAvailable: (CDVInvokedUrlCommand*)command;
